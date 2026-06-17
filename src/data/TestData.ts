d41c6466e71e42e08530efdbb98f8c8e036c1e08
NdabeMazii <ndabezinhlemazibuko6@gmail.com>
2026-06-17 23:40:08 +0200

    Coded TestData and BasePage

---
+++ src/data/TestData.ts
@@ -0,0 +1,21 @@
+export const validUsers = {
+    admin: {
+        username: 'admin@gmail.com',
+        password: '@12345678',
+        name: 'Nkosi'
+    },
+
+    classUser: {
+        username: 'ndabemazii@gmail.com',
+        password: 'Mazii@053103',
+        name: 'Ndabezinhle'
+    },
+};
+
+export const invalidUsers = {
+    invalidEmail: {
+        username: 'thuthumazii',
+        password: 'Mazii@053103',
+        name: 'Thuthuka'
+    },
+};    
