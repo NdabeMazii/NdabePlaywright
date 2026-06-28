fcee8f63be70745d75af66355bd413fe2d43662b
NdabeMazii <ndabezinhlemazibuko6@gmail.com>
2026-06-28 11:03:56 +0200

    Added soft and hard assertions

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,8 +2,11 @@ import {expect, test} from "../src/fixtures/CustomFixtures";
 import {validUsers, invalidUsers} from "../src/data/TestData";
 
 
-test('Positive login - Admin', async({ loginPage, homePage }) => {
+test('Positive login - Admin', async({ loginPage, homePage, page }) => {
     await loginPage.basePageGoToUrl('*/');
     await loginPage.navigateToLoginPage();
     await loginPage.userLogin(validUsers.admin.username, validUsers.admin.password);
+    //soft assertion
+    await expect.soft(page).toHaveURL(/dashboard/);
+    await homePage.verifyHomePageIsDisplayed();
 });
